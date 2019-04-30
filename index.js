8e4719bfb15703ee2217c282da818dafe97990f3
Leonardo Pires <leonardo@qbdvision.com>
2019-04-30 15:20:14 -0300

    QI-2416 - Added index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import EXCIPIENTS_GUIDANCE from "./guidance-for-industry/2002d-0389-gdl0002.pdf";
+
+export const ReferenceDocuments = {
+  EXCIPIENTS_GUIDANCE,
+};
+
+export default ReferenceDocuments;
