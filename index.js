73bf2c6928698189e7758d3ab9df1e45f1ce5f49
afkhalid <ahmed.motivated@gmail.com>
2019-10-01 21:36:56 +0200

    Q9_GuideLine.pdf

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import EXCIPIENTS_GUIDANCE from "./guidance-for-industry/2002d-0389-gdl0002.pdf";
+import Q9_GUIDELINE from "./guidance-for-industry/Q9_Guideline.pdf";
 
 export const ReferenceDocuments = {
   EXCIPIENTS_GUIDANCE,
+  Q9_GUIDELINE
 };
 
 export default ReferenceDocuments;
