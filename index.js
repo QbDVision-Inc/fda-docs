9dfd4cacbfea17ac6301a39cd4572500277519aa
Ryan Shillington <ryan@cherrycirclesoftware.com>
2019-10-08 09:47:09 -0400

    Adding the Q11 Guideline

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import EXCIPIENTS_GUIDANCE from "./guidance-for-industry/2002d-0389-gdl0002.pdf";
 import Q9_GUIDELINE from "./guidance-for-industry/Q9_Guideline.pdf";
+import Q11_GUIDELINE from "./guidance-for-industry/Q11_Guideline.pdf";
 
 export const ReferenceDocuments = {
   EXCIPIENTS_GUIDANCE,
-  Q9_GUIDELINE
+  Q9_GUIDELINE,
+  Q11_GUIDELINE,
 };
 
 export default ReferenceDocuments;
