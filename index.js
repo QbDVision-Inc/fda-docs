6f2f8511efea8106f1c54a59c53a19c6e213349f
afkhalid <ahmed.motivated@gmail.com>
2019-12-26 12:32:39 +0200

    Adding more docs Q8_R2_Pharmaceutical_71535.pdf ANDA_Submissions_Guidance_102360.pdf Target_Product_Profile_A_72566.pdf

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import EXCIPIENTS_GUIDANCE from "./guidance-for-industry/2002d-0389-gdl0002.pdf";
 import Q9_GUIDELINE from "./guidance-for-industry/Q9_Guideline.pdf";
 import Q11_GUIDELINE from "./guidance-for-industry/Q11_Guideline.pdf";
+import Q8_GUIDELINE from "./guidance-for-industry/Q8_R2_Pharmaceutical_71535.pdf";
+import ANDA_Submissions_Guidance from "./guidance-for-industry/ANDA_Submissions_Guidance_102360.pdf";
+import Target_Product_Profile_A from "./guidance-for-industry/Target_Product_Profile_A_72566.pdf";
 
 export const ReferenceDocuments = {
   EXCIPIENTS_GUIDANCE,
+  Q8_GUIDELINE,
   Q9_GUIDELINE,
   Q11_GUIDELINE,
+  Target_Product_Profile_A,
+  ANDA_Submissions_Guidance,
 };
 
 export default ReferenceDocuments;
