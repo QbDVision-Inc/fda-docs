032fe75c997b40a1c651447e4060806463e25b2a
Ryan Shillington <ryan@cherrycirclesoftware.com>
2020-06-19 13:15:30 -0400

    Updating to include FDA 21 CFR Part 11 guidance because ecfr.gov now has a captcha :-(.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fda-docs
 Hosts FDA documents.
+
+# To add a doc
+* Add the pdf to the `guidance-for-industry` directory
+* Add it to index.js to be packaged
+* Increase the version in package.json
+* Publish a new version
+  * `npm publish .`
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import EXCIPIENTS_GUIDANCE from "./guidance-for-industry/2002d-0389-gdl0002.pdf";
 import Q9_GUIDELINE from "./guidance-for-industry/Q9_Guideline.pdf";
 import Q11_GUIDELINE from "./guidance-for-industry/Q11_Guideline.pdf";
+import FDA_21CFR_PART_11 from "./guidance-for-industry/21CFR-Part-11.pdf";
 
 export const ReferenceDocuments = {
   EXCIPIENTS_GUIDANCE,
   Q9_GUIDELINE,
   Q11_GUIDELINE,
+  FDA_21CFR_PART_11,
 };
 
 export default ReferenceDocuments;
